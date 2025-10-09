62da8757ffab52437b0f3ca4dd313a51280dc4e3
Hakan Fıstık <hakam.fstk@gmail.com>
2025-10-09 10:35:25 +0200

    Add project files.

---
+++ TunitBug/Program.cs
@@ -0,0 +1,9 @@
+﻿internal sealed class UnitTest
+{
+    [Test]
+    [Arguments("1", "2")]
+    // make the method public and test will be discovered
+    internal void TestMethod(string s1, string s2)
+    {
+    }
+}
